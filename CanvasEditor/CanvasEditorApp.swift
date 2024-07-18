ec49fe85f213f43eb9e0d794e4212f229d58e206
ailu <im.ailu@outlook.com>
2024-07-18 08:44:51 +0800

    Initial Commit

---
+++ CanvasEditor/CanvasEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanvasEditorApp.swift
+//  CanvasEditor
+//
+//  Created by ailu on 2024/7/18.
+//
+
+import SwiftUI
+
+@main
+struct CanvasEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CanvasEditorTests/CanvasEditorTests.swift
@@ -0,0 +1,17 @@
+//
+//  CanvasEditorTests.swift
+//  CanvasEditorTests
+//
+//  Created by ailu on 2024/7/18.
+//
+
+import Testing
+@testable import CanvasEditor
+
+struct CanvasEditorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
